63f9c881c172d7fadde4e76ef027ebb05a3e844d
ayush123-bit <ayushrai803@gmail.com>
2025-08-01 14:40:59 +0530

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,7 +40,7 @@ const googleLogin = async (req, res) => {
     res.cookie('ZeroXtoken', jwtToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'lax',
       maxAge: 3600000 // 1 hour
     });
 
